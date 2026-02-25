41bbcb6619296a123796a394d9e022e2c81edf66
Sascha Skulima <218423+Skulli@users.noreply.github.com>
2026-02-25 09:47:52 +0100

    chg:dev: vibe coding für Berliner Bezirke

--- spec/lib/zip-codes_spec.rb
+++ spec/lib/zip-codes_spec.rb
@@ -18,4 +18,8 @@ RSpec.describe ZipCodes do
   it "exact entry" do
     expect(described_class.identify("20535")).to eq({city: "Hamburg", code: "20535", county: nil, osm_id: "62782", state: "Hamburg"})
   end
+
+  it "exact entry with bezirk" do
+    expect(described_class.identify("13357")).to eq({city: "Berlin", code: "13357", county: "Mitte-Wedding-Tiergarten", osm_id: "62422", state: "Berlin"})
+  end
 end
