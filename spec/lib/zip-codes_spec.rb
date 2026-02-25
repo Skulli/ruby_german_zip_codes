cec18de6e650af627930e93c9df94fb2d3e974be
Sascha Skulima <218423+Skulli@users.noreply.github.com>
2026-02-25 10:20:27 +0100

    chg:dev: Hamburger Bezirke ergänzt

--- spec/lib/zip-codes_spec.rb
+++ spec/lib/zip-codes_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe ZipCodes do
   end
 
   it "exact entry" do
-    expect(described_class.identify("20535")).to eq({city: "Hamburg", code: "20535", county: nil, osm_id: "62782", state: "Hamburg"})
+    expect(described_class.identify("20535")).to eq({city: "Hamburg", code: "20535", county: "Hamburg-Mitte", osm_id: "62782", state: "Hamburg"})
   end
 
   it "exact entry with bezirk" do
